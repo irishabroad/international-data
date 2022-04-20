f28fdeadfa04ca560e1463419f47e45c9de1f40c
joe <joe@home.com>
2022-04-20 23:26:12 +0100

    Utility methods

---
+++ utils.js
@@ -0,0 +1,18 @@
+const dayjs = require('dayjs');
+
+/**
+ * Convert a string in the format yyyy-MM-dd to a dayjs object at 12 noon
+ * @param  {string} date a string in the format yyyy-MM-dd
+ * @return {dayjs}	a dayjs object
+ */
+const toDayJs = (date) => dayjs(date + "T12:00:00");
+
+/**
+ * Return a JSON object representing the Ireland team in a game
+ * @param  game a game object
+ * @return a JSON object representing the Ireland team 
+ */
+const getIrelandFromGame = (game) => game.homeTeam.teamRef === 0 ? game.homeTeam : game.awayTeam;
+
+exports.toDayJs = toDayJs;
+exports.getIrelandFromGame = getIrelandFromGame;
