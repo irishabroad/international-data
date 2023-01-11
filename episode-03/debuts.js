b8062922e1f2f958ac0efc33b3048bcf60e7dcce
joe <joe@home.com>
2023-01-11 17:19:42 +0000

    Episode 3-Chapter 1-2 Process players array

--- episode-03/debuts.js
+++ episode-03/debuts.js
@@ -1,10 +1,15 @@
-const players = require('../lib/players');
+const fs = require('fs');
+
 const games = require('../lib/games');
 const managers = require('../lib/managers');
 
-const playerId = parseInt(process.argv[2]);
-const player = players.getPlayerById(playerId);
-const caps = games.getPlayerCaps(player.id);
-const debut = caps[0];
-const manager = managers.getManagerByDate(debut.date);
-console.log(`${player.firstName} ${player.surName} was given his debut by ${manager.name}`);
+const players = JSON.parse(fs.readFileSync('../data/players.json', 'utf-8')).players;
+
+const getPlayerDebutManager = (player) => {
+	const caps = games.getPlayerCaps(player.id);
+	const debut = caps[0];
+	const manager = managers.getManagerByDate(debut.date);
+	console.log(`${player.firstName} ${player.surName} was given his debut by ${manager.name}`);
+};
+
+players.forEach(getPlayerDebutManager);
