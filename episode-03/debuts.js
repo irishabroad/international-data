360f03908f3de0da9cf4e75701419b5ec1ab338d
joe <joe@home.com>
2023-01-11 17:20:08 +0000

    Episode 3-Chapter 1-3 Add caps check

--- episode-03/debuts.js
+++ episode-03/debuts.js
@@ -7,9 +7,11 @@ const players = JSON.parse(fs.readFileSync('../data/players.json', 'utf-8')).pla
 
 const getPlayerDebutManager = (player) => {
 	const caps = games.getPlayerCaps(player.id);
-	const debut = caps[0];
-	const manager = managers.getManagerByDate(debut.date);
-	console.log(`${player.firstName} ${player.surName} was given his debut by ${manager.name}`);
+	if (caps.length > 0) {
+		const debut = caps[0];
+		const manager = managers.getManagerByDate(debut.date);
+		console.log(`${player.firstName} ${player.surName} was given his debut by ${manager.name}`);
+	}
 };
 
 players.forEach(getPlayerDebutManager);
