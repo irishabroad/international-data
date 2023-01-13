e562be81728ad7e2984a48d400d05a64b1f178d0
joe <joe@home.com>
2023-01-13 18:05:23 +0000

    Episode 3-Chapter 2-4 Age of each player in a starting XI

--- episode-03/startingXIAge.js
+++ episode-03/startingXIAge.js
@@ -1,13 +1,24 @@
-const dayjs = require('dayjs');
-
+const countries = require('../lib/countries');
+const games = require('../lib/games');
+const levels = require('../lib/levels');
 const players = require('../lib/players');
 const utils = require('../lib/utils');
 
-const playerId = parseInt(process.argv[2]);
-const dateId = process.argv[3];
+const gameId = parseInt(process.argv[2]);
 
-const player = players.getPlayerById(playerId);
-const dateOfBirth = utils.toDayJs(player.dateOfBirth);
-const date = utils.toDayJs(dateId);
-const age = date.diff(dateOfBirth, 'year');
-console.log(`${player.firstName} ${player.surName} was ${age} years old on ${date.format('dddd, D MMMM YYYY')}`);
+const calculatePlayerAge = (playerId, date) => {
+  const player = players.getPlayerById(playerId);
+  const dateOfBirth = utils.toDayJs(player.dateOfBirth);
+  const age = date.diff(dateOfBirth, 'year');
+  console.log(`${player.firstName} ${player.surName} was ${age} years old on ${date.format('dddd, D MMMM YYYY')}`);
+};
+
+const game = games.getGameById(gameId);
+const gameDate = utils.toDayJs(game.date);
+const dateString = gameDate.format('dddd, D MMMM YYYY');
+const levelName = levels.getLevelById(game.level).name;
+const homeTeamName = countries.getCountryById(game.homeTeam.teamRef).name;
+const awayTeamName = countries.getCountryById(game.awayTeam.teamRef).name;
+
+console.log(`${dateString} ${levelName} ${homeTeamName} ${game.homeTeam.scored}-${game.awayTeam.scored} ${awayTeamName}`);
+game.startingXI.forEach(sP => calculatePlayerAge(sP.playerRef, gameDate)); 
