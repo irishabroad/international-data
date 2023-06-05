b7aa72a1bf43606823b5a74634d7b1156d61e0c1
joe <joe@home.com>
2023-06-05 12:13:06 +0100

    Episode 4-1-2 Every player's first competitive start

--- episode-04/node.js
+++ episode-04/node.js
@@ -4,18 +4,26 @@ const levels = require('../lib/levels');
 const utils = require('../lib/utils');
 const countries = require('../lib/countries');
 
-const player = players.getPlayerById(369);
-const competitiveStarts = games.getPlayerStarts(player.id)
-                               .filter(g => levels.isCompetitive(g.level));
+const allPlayers = players.getAllPlayers();
 
-const firstCompetitiveStart = competitiveStarts[0];
-const dateOfBirth = utils.toDayJs(player.dateOfBirth);
-const firstCompetitiveStartDate = utils.toDayJs(firstCompetitiveStart.date);
-const age = firstCompetitiveStartDate.diff(dateOfBirth, 'day');
-const name = `${player.firstName} ${player.surName}`;
-const date = firstCompetitiveStartDate.format('dddd, D MMMM YYYY');
-const level = levels.getLevelById(firstCompetitiveStart.level).name;
-const homeTeam = countries.getCountryById(firstCompetitiveStart.homeTeam.teamRef).name;
-const score = `${firstCompetitiveStart.homeTeam.scored}-${firstCompetitiveStart.awayTeam.scored}`;
-const awayTeam = countries.getCountryById(firstCompetitiveStart.awayTeam.teamRef).name;
-console.log(`${name} was ${age} days old on his competitive debut, on ${date} in a ${level} that ended ${homeTeam} ${score} ${awayTeam}`);
+const firstCompetitiveStart = (player) => {
+    const competitiveStarts = games.getPlayerStarts(player.id)
+                                   .filter(g => levels.isCompetitive(g.level));
+
+    const firstCompetitiveStart = competitiveStarts[0];
+    const dateOfBirth = utils.toDayJs(player.dateOfBirth);
+    const firstCompetitiveStartDate = utils.toDayJs(firstCompetitiveStart.date);
+    const age = firstCompetitiveStartDate.diff(dateOfBirth, 'day');
+    return {
+        name: `${player.firstName} ${player.surName}`,
+        date: firstCompetitiveStartDate.format('dddd, D MMMM YYYY'),
+        age: age,
+        level: levels.getLevelById(firstCompetitiveStart.level).name,
+        homeTeam: countries.getCountryById(firstCompetitiveStart.homeTeam.teamRef).name,
+        score: `${firstCompetitiveStart.homeTeam.scored}-${firstCompetitiveStart.awayTeam.scored}`,
+        awayTeam: countries.getCountryById(firstCompetitiveStart.awayTeam.teamRef).name
+    };
+};
+
+const results = allPlayers.map(firstCompetitiveStart);
+console.table(results);
