b3040ae302f8a11d7b049fcc6b92553e7f7fa26b
joe <joe@home.com>
2023-06-05 12:13:54 +0100

    Episode 4-1-3 20 youngest competitive starts

--- episode-04/node.js
+++ episode-04/node.js
@@ -10,20 +10,29 @@ const firstCompetitiveStart = (player) => {
     const competitiveStarts = games.getPlayerStarts(player.id)
                                    .filter(g => levels.isCompetitive(g.level));
 
-    const firstCompetitiveStart = competitiveStarts[0];
-    const dateOfBirth = utils.toDayJs(player.dateOfBirth);
-    const firstCompetitiveStartDate = utils.toDayJs(firstCompetitiveStart.date);
-    const age = firstCompetitiveStartDate.diff(dateOfBirth, 'day');
-    return {
-        name: `${player.firstName} ${player.surName}`,
-        date: firstCompetitiveStartDate.format('dddd, D MMMM YYYY'),
-        age: age,
-        level: levels.getLevelById(firstCompetitiveStart.level).name,
-        homeTeam: countries.getCountryById(firstCompetitiveStart.homeTeam.teamRef).name,
-        score: `${firstCompetitiveStart.homeTeam.scored}-${firstCompetitiveStart.awayTeam.scored}`,
-        awayTeam: countries.getCountryById(firstCompetitiveStart.awayTeam.teamRef).name
-    };
+    let retVal = undefined;
+    if (competitiveStarts.length > 0) {
+        const firstCompetitiveStart = competitiveStarts[0];
+        const dateOfBirth = utils.toDayJs(player.dateOfBirth);
+        const firstCompetitiveStartDate = utils.toDayJs(firstCompetitiveStart.date);
+        const age = firstCompetitiveStartDate.diff(dateOfBirth, 'day');
+        retVal = {
+            name: `${player.firstName} ${player.surName}`,
+            date: firstCompetitiveStartDate.format('dddd, D MMMM YYYY'),
+            age: age,
+            level: levels.getLevelById(firstCompetitiveStart.level).name,
+            homeTeam: countries.getCountryById(firstCompetitiveStart.homeTeam.teamRef).name,
+            score: `${firstCompetitiveStart.homeTeam.scored}-${firstCompetitiveStart.awayTeam.scored}`,
+            awayTeam: countries.getCountryById(firstCompetitiveStart.awayTeam.teamRef).name
+        };
+    }
+    return retVal;
 };
 
-const results = allPlayers.map(firstCompetitiveStart);
+const results = allPlayers.filter(p => p.dateOfBirth)
+                          .map(firstCompetitiveStart)
+                          .filter(r => r !== undefined)
+                          .sort((r1, r2) => r1.age - r2.age)
+                          .slice(0, 20);
+
 console.table(results);
